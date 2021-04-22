fa37921c308b60cf95863318cbdfa0560ead8045
vladimirca2000 <vladimirca2000@gmail.com>
2021-04-21 23:27:53 -0300

    Navbar e Rodape - inicio

--- HibitSite/src/app/app-routing.module.ts
+++ HibitSite/src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { HomeComponent } from './home/home.component';
+
+const routes: Routes = [
+  {path: 'home', component: HomeComponent},
+
+  {path: '', redirectTo: 'home', pathMatch:'full'},
+  {path: '**', redirectTo: 'home', pathMatch:'full'},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
